343159f38d042b15440faeb2501ccceca26b1caa
btagrass@163.com <btagrass@163.com>
2024-01-01 18:00:13 +0800

    fix

--- svc/svc.go
+++ svc/svc.go
@@ -2,13 +2,9 @@ package svc
 
 import (
 	"strings"
-	"time"
 
-	"github.com/go-redis/redis/v8"
-	"github.com/patrickmn/go-cache"
 	"github.com/samber/do"
 	"github.com/spf13/cast"
-	"github.com/spf13/viper"
 )
 
 var (
@@ -25,25 +21,15 @@ func Use[T any]() T {
 }
 
 type Svc[M any] struct {
-	Local  *cache.Cache
-	Redis  *redis.Client
+	*Cache
 	prefix string
 }
 
 func NewSvc[M any](prefix string) *Svc[M] {
-	s := &Svc[M]{
-		Local: cache.New(cache.NoExpiration, 5*time.Minute),
+	return &Svc[M]{
+		Cache:  NewCache(),
+		prefix: prefix,
 	}
-	addr := viper.GetString("redis.addr")
-	if addr != "" {
-		s.Redis = redis.NewClient(&redis.Options{
-			Addr:     addr,
-			Password: viper.GetString("redis.password"),
-			DB:       viper.GetInt("redis.db"),
-		})
-	}
-	s.prefix = prefix
-	return s
 }
 
 func (s *Svc[M]) GetFullKey(keys ...any) string {
